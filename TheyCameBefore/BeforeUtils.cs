34632be67c62c6f46d69aa403586cc566a476626
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-22 11:19:02 +0100

    Add fuctionality to class BeforeUtils

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace TheyCameBefore
 {
-    public class BeforeUtils
+    public static class BeforeUtils
     {
-        
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T item) where T : IComparable<T>
+        {
+            return items.Where(i => i.CompareTo(item) < 0);
+        }
     }
 }
