b1d799053bd5185eb7d90ddf695e031cd2a73557
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-22 11:41:10 +0100

    Add class Guarda3<T>

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T>
+    {
+        
+    }
+}
