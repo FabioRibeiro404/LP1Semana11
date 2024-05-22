7da39cbacba1ec44c7223aabceb67515e5c74942
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-22 11:14:38 +0100

    Update class Checker to static

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -5,21 +5,11 @@ using System.Linq;
 
 namespace HowManyOfThisType
 {
-    public class Checker : IEnumerable<int>
+    public static class Checker
     {
         public static int HowManyOfType<T>(IEnumerable<object> items)
         {
             return items.OfType<T>().Count();
-        }
-
-        public IEnumerator<int> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
